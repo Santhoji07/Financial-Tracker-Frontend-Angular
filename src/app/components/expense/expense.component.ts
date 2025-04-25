f9319c85d0f6d99418bdea21c92b0bad51a8d209
Sakshi-karkera <sakshiskarkera1@gmail.com>
2025-04-25 23:31:13 +0530

    Add update expense component and update routing/modules

--- src/app/components/expense/expense.component.ts
+++ src/app/components/expense/expense.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ExpenseService } from '../../services/expense/expense.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-expense',
@@ -21,13 +22,16 @@ export class ExpenseComponent {
     'Travelling',
     'Other',
   ];
+  expenses: any;
   loading = false;
   constructor(
     private fb: FormBuilder,
     private expenseService: ExpenseService,
-    private message: NzMessageService
+    private message: NzMessageService,
+    private router: Router
   ) {}
   ngOnInit() {
+    this.getAllExpenses();
     this.expenseForm = this.fb.group({
       title: [null, Validators.required],
       amount: [null, Validators.required],
@@ -39,12 +43,37 @@ export class ExpenseComponent {
   submitForm() {
     this.expenseService.postExpense(this.expenseForm.value).subscribe(
       (res) => {
-        this.message.success('Expense posted suvvessfully', {
+        this.message.success('Expense posted successfully', {
           nzDuration: 5000,
         });
       },
       (error) => {
         this.message.error('Error while posting expense', { nzDuration: 5000 });
+      }
+    );
+  }
+
+  getAllExpenses() {
+    this.expenseService.getAllExpense().subscribe((res) => {
+      this.expenses = res;
+      console.log(this.expenses);
+    });
+  }
+  updateExpense(id: number) {
+    this.router.navigateByUrl(`/expense/${id}/edit`);
+  }
+  deleteExpense(id: number) {
+    this.expenseService.deleteExpense(id).subscribe(
+      (res) => {
+        this.message.success('Expense deleted successfully', {
+          nzDuration: 5000,
+        });
+        this.getAllExpenses();
+      },
+      (error) => {
+        this.message.error('Error while deleting expense', {
+          nzDuration: 5000,
+        });
       }
     );
   }
---
+++ src/app/components/update-expense/update-expense.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { ExpenseService } from '../../expense.service';
+
+@Component({
+  selector: 'app-update-expense',
+  standalone: false,
+  templateUrl: './update-expense.component.html',
+  styleUrl: './update-expense.component.scss',
+})
+export class UpdateExpenseComponent {
+  expenseForm!: FormGroup;
+  listOfCategory: any[] = [
+    'Education',
+    'Groceries',
+    'Health',
+    'Subscription',
+    'Takeaways',
+    'Clothing',
+    'Travelling',
+    'Other',
+  ];
+  expenses: any;
+  loading = false;
+  constructor(
+    private fb: FormBuilder,
+    private expenseService: ExpenseService,
+    private message: NzMessageService,
+    private router: Router
+  ) {}
+  ngOnInit() {
+    this.expenseForm = this.fb.group({
+      title: [null, Validators.required],
+      amount: [null, Validators.required],
+      date: [null, Validators.required],
+      category: [null, Validators.required],
+      description: [null, Validators.required],
+    });
+  }
+  submitForm() {}
+}

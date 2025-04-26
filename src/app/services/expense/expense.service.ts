36f96cbdbd6835f92bbe1c25bb660c6a644b1e94
Saviksha0108 <savixashetty@gmail.com>
2025-04-26 16:52:24 +0530

    My local changes

--- src/app/services/expense/expense.service.ts
+++ src/app/services/expense/expense.service.ts
@@ -10,4 +10,16 @@ export class ExpenseService {
   postExpense(expenseDTO: any): Observable<any> {
     return this.http.post(BASIC_URL + 'api/expense', expenseDTO);
   }
+
+  getAllExpenses():Observable<any>{
+    return this.http.get(BASIC_URL+ "api/expense/all");
+  }
+
+  deleteExpense(id: number): Observable<any>{
+    return this.http.delete(BASIC_URL + `api/expense/${id}` );
+  }
+
+  getAllExpenseById(id:number):Observable<any>{
+    return this.http.get(BASIC_URL+ `api/expense/${id}`);
+  }
 }
